f2c3c40e1990e8e46c402ddce67edf4d21c32dcf
Daniel White <33187756+dwhite07@users.noreply.github.com>
2017-10-30 11:15:59 -0400

    Rename project.py to Grade Calculator

--- project.py
+++
@@ -1,116 +0,0 @@
-#Variables
-project_sum = 0
-project_total = 700
-quizzes_sum = 0
-quizzes_total = 0
-final_average = 0
-
-#get scores for projects as floating point decimals
-project1_score = float(input("Enter your score for project 1 in decimal form: "))
-print('Your score for project 1:', project1_score)
-project_sum += project1_score
-
-
-project2_score = float(input("Enter your score for project 2 in decimal form: "))
-print('Your score for project 2:', project2_score)
-project_sum += project2_score
-
-
-project3_score = float(input("Enter your score for project 3 in decimal form: "))
-print('Your score for project 3:', project3_score)
-project_sum += project3_score
-
-
-project4_score = float(input("Enter your score for project 4 in decimal form: "))
-print('Your score for project 4:', project4_score)
-project_sum += project4_score
-
-
-project5_score = float(input("Enter your score for project 5 in decimal form: "))
-print('Your score for project 5:', project5_score)
-project_sum += project5_score
-
-
-project6_score = float(input("Enter your score for project 6 in decimal form: "))
-print('Your score for project 6:', project6_score)
-project_sum += project6_score
-
-
-project7_score = float(input("Enter your score for project 7 in decimal form: "))
-print('Your score for project 7:', project7_score)
-project_sum += project7_score
-
-
-#get scores for quizzes as floating point decimals
-quiz1_score = float(input("Enter your score for quiz 1 in decimal form: "))
-quiz1_total = float(input("Enter the total number of points quiz 1 was out of in decimal form: "))
-
-quizzes_sum += quiz1_score
-quizzes_total += quiz1_total
-quiz1_average = (quiz1_score / quiz1_total) * 100
-print('Your score for quiz 1:', quiz1_average)
-
-
-quiz2_score = float(input("Enter your score for quiz 2 in decimal form: "))
-quiz2_total = float(input("Enter the total number of points quiz 2 was out of in decimal form: "))
-
-quizzes_sum += quiz2_score
-quizzes_total += quiz1_total
-quiz2_average = (quiz2_score / quiz2_total) * 100
-print('Your score for quiz 2:', quiz2_average)
-
-
-quiz3_score = float(input("Enter your score for quiz 3 in decimal form: "))
-quiz3_total = float(input("Enter the total number of points quiz 3 was out of in decimal form: "))
-
-quizzes_sum += quiz3_score
-quizzes_total += quiz1_total
-quiz3_average = (quiz3_score / quiz3_total) * 100
-print('Your score for quiz 3:', quiz3_average)
-
-
-quiz4_score = float(input("Enter your score for quiz 4 in decimal form: "))
-quiz4_total = float(input("Enter the total number of points quiz 4 was out of in decimal form: "))
-
-quizzes_sum += quiz4_score
-quizzes_total += quiz1_total
-quiz4_average = (quiz4_score / quiz4_total) * 100
-print('Your score for quiz 4:', quiz4_average)
-
-
-#computes student average for projects
-project_average = project_sum / project_total
-quiz_average = quizzes_sum / quizzes_total
-
-#compute final sum
-final_sum = project_sum + quizzes_sum
-
-#compute final total
-final_total = quizzes_total + project_total
-
-#computes final average for the course
-final_average = (final_sum / final_total) * 100
-
-#prints final average
-print('Your final grade in this class out of 100 is', final_average)
-
-#assigns letter grade based on final average
-if (final_average >= 90) :
-    letter_grade = 'A'
-elif (final_average >= 80 and final_average < 90):
-    letter_grade = 'B'
-elif (final_average >= 70 and final_average < 80):
-    letter_grade = 'C'
-elif (final_average >= 60 and final_average < 70):
-    letter_grade = 'D'
-else:
-    letter_grade = 'F'
-
-#prints final letter grade in class
-print('Your letter grade in this class is: ', letter_grade)
-
-#prints whether or not student failed class
-if (letter_grade == 'F'):
-    print('You are failing this class')
-else:
-    print('You are passing this class!!!')
